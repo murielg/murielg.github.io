9b13f59ff8643b9643e60d25f2cc705d136261bf
Muriel Gonzalez <murielgonzalez@gmail.com>
2020-02-13 10:13:02 -0600

    Updates

--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from '../components/layout';
-const FourOhFourPage = () => (
-  <Layout>
-    <h1 style={{textAlign: 'center'}}>404 - Page Not Found</h1>
-  </Layout>
-);
-
-export default FourOhFourPage
--- src/pages/projects.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from '../components/layout';
-const ProjectsPage = () => (
-  <Layout>
-    <h1>Projects</h1>
-  </Layout>
-);
-
-export default ProjectsPage
