784f810c2f2e3a54ffd4e983f775d7deffcab92d
Muriel Gonzalez <murielgonzalez@gmail.com>
2020-08-14 16:39:07 -0500

    typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ const AboutPage = () => (
   <Layout>
     <div className='bio'>
       <p>Hey!</p><br/>
-      <p>My name is I'm Muriel Gonzalez, nice to meet you! </p><br/>
+      <p>My name is Muriel Gonzalez, nice to meet you! </p><br/>
       <p>📍 I live in Dallas, Texas and I love developing apps, websites and techy things 📱</p><br/>
       <p>I also love 😸cats, 🐶dogs, 🚲bikes, 🎮games and 🚙roadtrips<br/></p>
       <p>Check out some of my code on Github, and connect with me on LinkedIn✌️</p>
