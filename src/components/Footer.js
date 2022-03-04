278f1b7d04d4dc427fa8a2b33c0f532d8a7afaf0
Muriel Gonzalez <muriel.gonzalez@onxmaps.com>
2022-03-03 18:33:19 -0700

    unused

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { config } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 config.autoAddCss = false;
 const Footer = () => (
   <footer>
